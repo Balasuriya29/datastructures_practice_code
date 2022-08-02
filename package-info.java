079353baf505d91dd82cf1a256456d1b538d1579
Balasuriya <100402643+Balasuriya29@users.noreply.github.com>
2022-08-02 15:23:58 +0530

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -1,9 +1,18 @@
 /**
  * These is the Domain package which contains all the data structures build from scratch with their own logic
  * 
- * 1. DyanamicArray
- * 2. LinkedList
- * 3. Stack
+ * 1. DyanamicArray✅
+ * 2. LinkedList✅
+ * 3. Stack✅
+ * 4. Queue✅
+ * 5. HashTables✅
+ * 6. Binary Trees✅
+ * 7. AVL Trees✅
+ * 8. Heaps✅
+ * 9. Tries✅
+ * 10. Graphs
+ * 11. Unidirected Graphs
+ * 7. 
  */
 /**
  * 
